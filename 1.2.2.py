6adcdb78117b300eabe0de31fc5d021e50c73a30
himanshu21bcs11403 <125957041+himanshu21bcs11403@users.noreply.github.com>
2023-02-21 14:29:30 +0530

    Update 1.2.2.py

--- 1.2.2.py
+++ 1.2.2.py
@@ -1,9 +1,10 @@
 import math
 var = int(input("Enter the number : "))
 varstring = str(var)
+lengthvar = len(varstring)
 total = 0
 for i in range(0,len(varstring)):
-   total = total + math.pow(int(varstring[i]),3)
+   total = total + math.pow(int(varstring[i]),lengthvar)
 total=int(total)
 if (total == var ):
     print("Armstong number")
